5e69edddaf48d22fa7a63b310ba1cc34984737aa
jdj-design <doc_jones12@yahoo.com>
2025-02-04 15:58:01 -0500

    kinda got it

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const toDo = [];
 
 const btn = document.getElementById('btn');
 const texting = document.getElementById('texting');
-const printOut= document.getElementById('output1');
+const printOut = document.getElementById('output1');
+
+btn.addEventListener("click", () => {
+    const newItem = document.createElement('p');
+    newItem.textContent = texting.value;
+    printOut.appendChild(newItem);
+});
 
 
-btn.addEventListener("click", listing);
-
-function listing(){
-    let list = texting.value;
+/*function listing(){
+    const list = texting.value.trim();
     toDo.push(list);
     printOut.innerHTML=toDo;
     
+    
 };
-console.log(toDo);
+*/
+
