416e66b38e9cc3e36423724222988a8002bece0e
jdj-design <doc_jones12@yahoo.com>
2025-02-04 12:13:43 -0500

    adding java

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 console.log('My code is running');
+
+
+/* below will be the event for 
+clicking the add item*/
+
 const toDoList = [];
-(item) => (toDoList.push);
+const textField = document.getElementById("textBox");
+const addButton = document.getElementById("addItem");
+
+addButton.addEventListener("click", () =>{
+    const newEntry = textField.value;
+    toDoList.push(newEntry);
+})
