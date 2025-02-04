95ab5431a4668181d80ee06e2f36b20228252294
jdj-design <doc_jones12@yahoo.com>
2025-02-04 17:33:38 -0500

    didnt get far

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const btn = document.getElementById('btn');
 const texting = document.getElementById('texting');
 const printOut = document.getElementById('output1');
 
+
 btn.addEventListener("click", () => {
     const newItem = document.createElement('p');
+    const list = texting.value;
+    toDo.push(list);
     newItem.textContent = texting.value;
     printOut.appendChild(newItem);
 });
+
+// strikeout//
+
 
 
 /*function listing(){
